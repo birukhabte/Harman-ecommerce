42ad91fb93ec992d148ba578747d1e048eb218e1
biruk habte <biruk.habte-ug@aau.edu.et>
2025-10-01 20:49:00 +0300

    Update banner images

---
+++ Client/app/cart-confirmation/[status]/page.tsx
@@ -0,0 +1,10 @@
+import CartConfirmation from '@/components/Checkout/CartConfirmation'
+import React from 'react'
+
+const page = () => {
+  return (
+    <CartConfirmation/>
+  )
+}
+
+export default page// Updated on 2025-10-01 20:49:00
