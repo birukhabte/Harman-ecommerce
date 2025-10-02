289befa78cd1f65b8b00f8136d08bd1867fb2b2a
biruk habte <biruk.habte-ug@aau.edu.et>
2025-10-02 12:21:00 +0300

    Add product recommendations

---
+++ Client/next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: false,
+    images: {
+        remotePatterns: [],
+    },
+    output:'standalone'
+};
+
+export default nextConfig;
+// Updated on 2025-10-02 12:21:00
