c9ef5637ae32ebc50bfc340a9fc1d74000ea9bc7
biruk habte <biruk.habte-ug@aau.edu.et>
2025-10-08 23:26:00 +0300

    Improve mobile responsiveness

---
+++ Client/app/product/[productID]/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import Common from '@/components/CommonPage/Common'
+import ProductPage from '@/components/ProductUi/ProductPage'
+import React from 'react'
+
+const page = () => {
+  return (
+    <Common Component={ProductPage}/>
+  )
+}
+
+export default page// Updated on 2025-10-08 23:26:00
