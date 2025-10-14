165fbf44107bc2b21b72228de7f7f9131dbdb451
biruk habte <biruk.habte-ug@aau.edu.et>
2025-10-14 16:24:00 +0300

    Fix session handling

---
+++ Client/app/categories/[category]/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+import Common from '@/components/CommonPage/Common'
+import React from 'react'
+import CategoryPage from '@/components/Categories/CategoryPage'
+
+const page = () => {
+  return (
+    <Common Component={CategoryPage}/>
+  )
+}
+
+export default page// Updated on 2025-10-14 16:24:00
